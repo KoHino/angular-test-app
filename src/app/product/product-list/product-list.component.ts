54c253001147680de5949d35290fa298c8419219
Ko Hino <ko@LAB0002N.benextlab.local>
2022-05-09 16:20:12 +0900

    フロントエンドとバックエンドの結合

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { products } from 'src/app/products';
+import { ProductService } from '../shared/product.service';
 
 @Component({
   selector: 'app-product-list',
@@ -10,10 +10,19 @@ export class ProductListComponent implements OnInit {
 
   products: any
 
-  constructor() { }
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
-    this.products = products
-  }
+    // this.products = this.productService.getProducts()
 
+    const productObservable = this.productService.getProducts()
+    productObservable.subscribe(
+      (data) => {
+        this.products = data;
+        console.log('get value ' + data);
+      },
+      (err) => { console.error('something wrong occured: ' + err); },
+      () => { }
+    )
+  }
 }
